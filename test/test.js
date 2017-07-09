b7f9a7963a49596bad57910b583d2b5d1d10f426
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2017-07-09 18:36:32 +0900

    Add tests

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const test = require('tape');
 const { spawnLambda } = require('..');
 
 test('test', t => {
-  t.plan(4);
+  t.plan(8);
 
   const options = {
     dir: __dirname + '/lambda',
@@ -15,7 +15,7 @@ test('test', t => {
   const context = {
   };
 
-  spawnLambda(options, event, context, (err, result) => {
+  const lambdaProcess = spawnLambda(options, event, context, (err, result) => {
     t.error(err);
 
     t.equal(typeof(result), 'object');
@@ -25,6 +25,36 @@ test('test', t => {
 
     t.equal(event.test, 'test');
   });
+
+  t.equal(typeof(lambdaProcess), 'object');
+  t.equal(lambdaProcess.stdin, null);
+  t.equal(lambdaProcess.stdout, null);
+  t.equal(lambdaProcess.stderr, null);
+});
+
+test('test options.stdio', t => {
+  t.plan(5);
+
+  const options = {
+    dir: __dirname + '/lambda',
+    stdio: ['pipe', 'pipe', 'pipe'],
+  };
+
+  const event = {
+    test: 'test',
+  };
+
+  const context = {
+  };
+
+  const lambdaProcess = spawnLambda(options, event, context, (err, result) => {
+    t.error(err);
+  });
+
+  t.equal(typeof(lambdaProcess), 'object');
+  t.equal(lambdaProcess.stdin, null);
+  t.notEqual(lambdaProcess.stdout, null);
+  t.notEqual(lambdaProcess.stderr, null);
 });
 
 test('test options.moduleDir', t => {
