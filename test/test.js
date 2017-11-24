af02d34b04fc2af0145e4982b0d6289006a82ae5
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2017-11-24 22:43:21 +0900

    Change to EventEmitter based API

--- test/test.js
+++ test/test.js
@@ -1,146 +1,205 @@
 const test = require('tape');
-const { spawnLambda } = require('..');
+const {
+  spawnLambda,
+  INIT_RESULT,
+} = require('..');
 
 test('test', t => {
-  t.plan(8);
+  t.plan(9);
 
   const options = {
+    arn: t.name,
     dir: __dirname + '/lambda',
   };
 
-  const event = {
-    test: 'test',
-  };
-
-  const context = {
-  };
-
-  const lambdaProcess = spawnLambda(options, event, context, (err, result) => {
-    t.error(err);
-
-    t.equal(typeof(result), 'object');
-
-    const { event } = result;
-    t.equal(typeof(event), 'object');
-
-    t.equal(event.test, 'test');
-  });
+  const lambdaProcess = spawnLambda(options);
 
   t.equal(typeof(lambdaProcess), 'object');
   t.equal(lambdaProcess.stdin, null);
   t.equal(lambdaProcess.stdout, null);
   t.equal(lambdaProcess.stderr, null);
+
+  lambdaProcess.on(INIT_RESULT, ({ err }) => {
+    t.error(err);
+
+    const event = {
+      test: 'test',
+    };
+
+    const context = {
+    };
+
+    lambdaProcess.invoke(event, context, (err, result) => {
+      t.error(err);
+
+      t.equal(typeof(result), 'object');
+
+      const { event } = result;
+      t.equal(typeof(event), 'object');
+
+      t.equal(event.test, 'test');
+
+      lambdaProcess.kill();
+    });
+  });
 });
 
 test('test options.stdio', t => {
-  t.plan(5);
+  t.plan(7);
 
   const options = {
+    arn: t.name,
     dir: __dirname + '/lambda',
     stdio: ['pipe', 'pipe', 'pipe'],
   };
 
-  const event = {
-    test: 'test',
-  };
-
-  const context = {
-  };
-
-  const lambdaProcess = spawnLambda(options, event, context, (err, result) => {
-    t.error(err);
-  });
+  const lambdaProcess = spawnLambda(options);
 
   t.equal(typeof(lambdaProcess), 'object');
   t.equal(lambdaProcess.stdin, null);
   t.notEqual(lambdaProcess.stdout, null);
   t.notEqual(lambdaProcess.stderr, null);
+
+  lambdaProcess.on(INIT_RESULT, ({ err }) => {
+    t.error(err);
+
+    const event = {
+      test: 'test',
+    };
+
+    const context = {
+    };
+
+    lambdaProcess.invoke(event, context, (err, result) => {
+      t.error(err);
+
+      t.equal(typeof(result), 'object');
+
+      lambdaProcess.kill();
+    });
+  });
 });
 
 test('test options.moduleDir', t => {
-  t.plan(2);
+  t.plan(3);
 
   const options = {
+    arn: t.name,
     dir: __dirname,
     moduleDir: __dirname + '/lambda',
   };
 
-  const event = {
-    test: 'test',
-  };
+  const lambdaProcess = spawnLambda(options);
 
-  const context = {
-  };
-
-  spawnLambda(options, event, context, (err, result) => {
+  lambdaProcess.on(INIT_RESULT, ({ err }) => {
     t.error(err);
 
-    t.equal(typeof(result), 'object');
+    const event = {
+      test: 'test',
+    };
+
+    const context = {
+    };
+
+    lambdaProcess.invoke(event, context, (err, result) => {
+      t.error(err);
+
+      t.equal(typeof(result), 'object');
+
+      lambdaProcess.kill();
+    });
   });
 });
 
 test('test options.babel', t => {
-  t.plan(2);
+  t.plan(3);
 
   const options = {
+    arn: t.name,
     dir: __dirname + '/babel-lambda',
     babel: true,
   };
 
-  const event = {
-    test: 'test',
-  };
+  const lambdaProcess = spawnLambda(options);
 
-  const context = {
-  };
-
-  spawnLambda(options, event, context, (err, result) => {
+  lambdaProcess.on(INIT_RESULT, ({ err }) => {
     t.error(err);
 
-    t.equal(typeof(result), 'object');
+    const event = {
+      test: 'test',
+    };
+
+    const context = {
+    };
+
+    lambdaProcess.invoke(event, context, (err, result) => {
+      t.error(err);
+
+      t.equal(typeof(result), 'object');
+
+      lambdaProcess.kill();
+    });
   });
 });
 
 test('test options.lambdaEnv', t => {
-  t.plan(2);
+  t.plan(3);
 
   const options = {
+    arn: t.name,
     dir: __dirname + '/lambda',
     lambdaEnv: {
       foo: 'bar',
     },
   };
 
-  const event = {
-    test: 'test',
-  };
+  const lambdaProcess = spawnLambda(options);
 
-  const context = {
-  };
-
-  spawnLambda(options, event, context, (err, result) => {
+  lambdaProcess.on(INIT_RESULT, ({ err }) => {
     t.error(err);
 
-    t.equal(result.env.foo, 'bar');
+    const event = {
+      test: 'test',
+    };
+
+    const context = {
+    };
+
+    lambdaProcess.invoke(event, context, (err, result) => {
+      t.error(err);
+
+      t.equal(result.env.foo, 'bar');
+
+      lambdaProcess.kill();
+    });
   });
 });
 
 test('test options.additionalNodePath', t => {
-  t.plan(1);
+  t.plan(2);
 
   const options = {
+    arn: t.name,
     dir: __dirname + '/lambda2',
     additionalNodePath: __dirname + '/additional_node_modules',
   };
 
-  const event = {
-    test: 'test',
-  };
+  const lambdaProcess = spawnLambda(options);
 
-  const context = {
-  };
-
-  spawnLambda(options, event, context, (err, result) => {
+  lambdaProcess.on(INIT_RESULT, ({ err }) => {
     t.error(err);
+
+    const event = {
+      test: 'test',
+    };
+
+    const context = {
+    };
+
+    lambdaProcess.invoke(event, context, (err, result) => {
+      t.error(err);
+
+      lambdaProcess.kill();
+    });
   });
 });
